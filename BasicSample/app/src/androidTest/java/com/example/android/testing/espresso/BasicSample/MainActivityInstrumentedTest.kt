05dc178cb8c46e44516f99c05d44bf95a19623b3
awais1379 <awais.ali1379@gmail.com>
2024-11-23 21:41:47 -0500

    Activity Change Button Test Added

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
@@ -4,6 +4,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -27,6 +28,52 @@ class MainActivityInstrumentedTest {
             editText.setText("")
             changeTextButton.performClick()
             assertEquals("", textView.text.toString())
+        }
+    }
+
+    @Test
+    fun testOpenActivityChangeTextButton() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { activity ->
+            val editText = activity.findViewById<EditText>(R.id.editTextUserInput)
+            val openActivityButton = activity.findViewById<Button>(R.id.activityChangeTextBtn)
+
+            editText.setText("123")
+            openActivityButton.performClick()
+        }
+
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = ShowTextActivity.newStartIntent(context, "123")
+        ActivityScenario.launch<ShowTextActivity>(intent).onActivity { showTextActivity ->
+            val showTextView = showTextActivity.findViewById<TextView>(R.id.show_text_view)
+            assertEquals("123", showTextView.text.toString())
+        }
+
+        ActivityScenario.launch(MainActivity::class.java).onActivity { activity ->
+            val editText = activity.findViewById<EditText>(R.id.editTextUserInput)
+            val openActivityButton = activity.findViewById<Button>(R.id.activityChangeTextBtn)
+
+            editText.setText("abcdef")
+            openActivityButton.performClick()
+        }
+
+        val intent2 = ShowTextActivity.newStartIntent(context, "abcdef")
+        ActivityScenario.launch<ShowTextActivity>(intent2).onActivity { showTextActivity ->
+            val showTextView = showTextActivity.findViewById<TextView>(R.id.show_text_view)
+            assertEquals("abcdef", showTextView.text.toString())
+        }
+
+        ActivityScenario.launch(MainActivity::class.java).onActivity { activity ->
+            val editText = activity.findViewById<EditText>(R.id.editTextUserInput)
+            val openActivityButton = activity.findViewById<Button>(R.id.activityChangeTextBtn)
+
+            editText.setText("")
+            openActivityButton.performClick()
+        }
+
+        val intent3 = ShowTextActivity.newStartIntent(context, "")
+        ActivityScenario.launch<ShowTextActivity>(intent3).onActivity { showTextActivity ->
+            val showTextView = showTextActivity.findViewById<TextView>(R.id.show_text_view)
+            assertEquals("", showTextView.text.toString())
         }
     }
 }
